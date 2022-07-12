e123cb99605d9fa8f97c947687b12172a69b7902
Aleksandr Lukashov <a.lukashov@ejaw.net>
2022-07-12 17:36:17 +0300

    Create New Tiles
    JCP-4

--- src/scripts/game/scenes/Example2.ts
+++ src/scripts/game/scenes/Example2.ts
@@ -2,12 +2,15 @@ import { Data, GameObjects } from "phaser";
 import { WIDTH, CENTER_X, CENTER_Y } from "scripts/util/globals";
 import { Popup } from "./Example2/CustomPopup";
 import { List } from "scripts/util/extra";
+import TileManager  from "./Example2/TileManager";
 
 export default class Example2 extends Phaser.Scene {
     static grid_x_size = 8;
     static grid_y_size = 8;
 
-    public grid: Phaser.Geom.Point[][] = [];
+    public grid: Phaser.Math.Vector3[][] = [];
+    
+    private tilemanager: TileManager;
 
     constructor() {
         super({ key: "Example2" });
@@ -45,18 +48,26 @@ export default class Example2 extends Phaser.Scene {
             if (this.grid[grid_y] === undefined) {
                 this.grid[grid_y] = [];
             }
-            this.grid[grid_y].push(new Phaser.Geom.Point(data.x, data.y));
+            this.grid[grid_y].push(new Phaser.Math.Vector3(data.x, data.y, index));
         });
     }
 
     public create() {
         this.create_blank_grid();
+        this.createTileManager();
+        this.tilemanager.createTile();
+        this.tilemanager.createTile();
+    }
+
+    public createTileManager(){
+        this.tilemanager = new TileManager(this, Example2.grid_x_size, Example2.grid_y_size, this.grid);
     }
 
     public create_blank_grid() {
         this.grid.flat().forEach((element) => {
             const image = this.add.image(element.x, element.y, "ui-emptytile");
             image.setScale(0.5);
+            image.setDepth(0);
         });
     }
 }
---
+++ src/scripts/game/scenes/Example2/TileManager.ts
@@ -0,0 +1,91 @@
+export default class TileManager extends Phaser.GameObjects.Group {
+    private cols: number;
+    private rows: number;
+    private grid: Phaser.Math.Vector3[][];
+    private rnd = new Phaser.Math.RandomDataGenerator([
+        `${Phaser.Math.Between(0, 1000)}`,
+    ]);
+
+    constructor(
+        scene: Phaser.Scene,
+        cols: number,
+        rows: number,
+        grid: Phaser.Math.Vector3[][]
+    ) {
+        super(scene);
+
+        this.cols = cols;
+        this.rows = rows;
+        this.grid = grid;
+
+        this.initTiles();
+        this.scene.add.existing(this);
+    }
+
+    public createTile() {
+        const positionsAll = this.grid.flat().map((element) => element.z);
+        const childs = this.getMatching("active", true);
+        const positionsBusy = childs.map((element: Tile) => {
+            return element.grid_position;
+        });
+        
+        const positionsAllPhaser = new Phaser.Structs.Set(positionsAll);
+        const positionsBusyPhaser = new Phaser.Structs.Set(positionsBusy);
+        const positionsFreePhaser =
+            positionsAllPhaser.difference(positionsBusyPhaser);
+        
+        const tileid = this.rnd.pick(positionsFreePhaser.getArray());
+        const newTile = this.getFirstDead() as Tile;
+        newTile.setGridPosition(tileid, this.grid, this.cols, this.rows);
+        newTile.activate();
+    }
+
+    private destroyTile(x: number, y: number) {
+        const tile = this.get(x, y);
+        tile?.clear();
+    }
+
+    private initTiles() {
+        for (let i = 0; i < this.cols * this.rows; i++) {
+            this.add(new Tile(this.scene));
+        }
+    }
+}
+
+class Tile extends Phaser.GameObjects.Image {
+    public grid_position = -1;
+
+    constructor(scene: Phaser.Scene) {
+        super(scene, 0, 0, "tiles", 0);
+        this.setScale(0.5);
+        this.clear();
+        this.setDepth(1);
+        this.scene.add.existing(this);
+    }
+
+    public setGridPosition(
+        position: number,
+        grid: Phaser.Math.Vector3[][],
+        cols: number,
+        rows: number
+    ) {
+        this.grid_position = position;
+        this.x = grid[Math.floor(position / cols)][position % rows].x;
+        this.y = grid[Math.floor(position / cols)][position % rows].y;
+    }
+
+    public clearGridPosition() {
+        this.grid_position = -1;
+    }
+
+    public clear() {
+        this.setVisible(false);
+        this.setActive(false);
+        this.clearGridPosition();
+    }
+
+    public activate() {
+        this.setVisible(true);
+        this.setActive(true);
+    }
+}
